0e09c51594b795fbbbc057c7d5e1db2838424028
luiz-gabriel34 <luizgabi341765@gmail.com>
2025-08-15 08:00:57 -0300

    Update bimestral.c

--- bimestral.c
+++ bimestral.c
@@ -14,16 +14,12 @@ int main(){
 
 2.questão:
 #include <stdio.h>
-#include <stdlib.h>
 
 
 int main(){
-
-    int n1 = 0;
-    printf("Informe um numero: ");
-    scanf("%d", &n1);
-    printf(" %d \n %d %d \n %d %d %d \n %d %d %d %d \n %d %d %d %d %d \n %d %d %d %d %d %d %d \n ", n1, n1 + 1, n1 + 2, n1 + 3, n1 + 4, n1 + 5, n1 +6, n1 + 7, n1 + 8, n1 + 9 , n1 + 10, n1 + 11, n1 + 12, n1 + 13, n1 + 14, n1 + 15, n1 + 16, n1 + 16, n1 + 17, n1 + 18, n1 + 19, n1 + 20, n1 + 21);
-
+    for (int n = 0; n <= 10; n++){
+        printf("Numero \t Quadrado \t Cubo \n %d \t %d \t  \t %d \n", n, n * n, n * n * n);
+    }
 }
 3.questão:
 #include <stdio.h>
